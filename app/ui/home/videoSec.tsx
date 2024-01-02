837255356387a1b99dce760a8519663c23dfb5a3
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-01-02 17:44:44 +0530

    Made video responsive

--- app/ui/home/videoSec.tsx
+++ app/ui/home/videoSec.tsx
@@ -1,8 +1,8 @@
 export default function VideoSec() {
-    return(<div className="my-11 mx-auto w-full max-w-6xl flex flex-col items-center">
-        <div className="text-3xl sm:text-6xl font-bold text-center">What is Placed?</div>
-        <div className="p-6 w-full">
-        <iframe width="560" className="w-[80%] mx-auto h-[300px] sm:h-[500px]" height="500" src="https://www.youtube.com/embed/X6CsbhSVUEc?si=tmi-RFpeDDVjJ4Y5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+    return(<div className="my-11 mx-auto w-full max-w-6xl flex flex-col items-center px-11">
+        <div className="text-3xl sm:text-6xl font-bold text-center my-4">What is Placed?</div>
+        <div className="relative overflow-hidden pt-[66.6%] w-full">
+        <iframe width="560" className="absolute top-0 bottom-0 left-0 right-0 w-full h-full" height="316" src="https://www.youtube.com/embed/X6CsbhSVUEc?si=tmi-RFpeDDVjJ4Y5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
     </div>);
 }
