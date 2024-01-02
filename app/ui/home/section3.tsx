ece13ba50470468a3c3772192df54cd87710a247
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-01-02 17:34:32 +0530

    Fixed issue in vertical scroll

--- app/ui/home/section3.tsx
+++ app/ui/home/section3.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 
 export default function Section3() {
-    return (<div className="grid grid-cols-[1fr] min-[1000px]:grid-cols-[2fr_3fr] px-4 mx-auto max-w-6xl my-20">
-        <div className="text-3xl sm:text-5xl font-bold tracking-wider text-center">Techn Stack you will Build</div>
-        <div className="flex justify-center items-stretch py-2 h-28">
-            <Image src="/Images/unnamed.png"   className="mx-2 w-auto" width="100" height="100" alt="Tech Stack"/>
-            <Image src="/Images/unnamed-2.png" className="mx-2 w-auto" width="100" height="100" alt="Tech Stack"/>
-            <Image src="/Images/unnamed-3.png" className="mx-2 w-auto" width="100" height="100" alt="Tech Stack"/>
-            <Image src="/Images/unnamed-4.png" className="mx-2 w-auto" width="100" height="100" alt="Tech Stack"/>
-            <Image src="/Images/unnamed-5.png" className="mx-2 w-auto" width="100" height="100" alt="Tech Stack"/>
+    return (<div className="grid grid-cols-[1fr] md:grid-cols-[2fr_3fr] px-4 mx-auto max-w-6xl my-20">
+        <div className="text-3xl sm:text-5xl font-bold tracking-wider text-center px-2">Tech Stack you will Build</div>
+        <div className="flex flex-nowrap justify-center items-stretch py-2 h-auto lg:h-28 w-full">
+            <Image src="/Images/unnamed.png"   className="mx-1 w-1/6" width="100" height="100" alt="Tech Stack"/>
+            <Image src="/Images/unnamed-2.png" className="mx-1 w-1/6" width="100" height="100" alt="Tech Stack"/>
+            <Image src="/Images/unnamed-3.png" className="mx-1 w-1/6" width="100" height="100" alt="Tech Stack"/>
+            <Image src="/Images/unnamed-4.png" className="mx-1 w-1/6" width="100" height="100" alt="Tech Stack"/>
+            <Image src="/Images/unnamed-5.png" className="mx-1 w-1/6" width="100" height="100" alt="Tech Stack"/>
         </div>
     </div>);
 }
--- app/ui/home/videoSec.tsx
+++ app/ui/home/videoSec.tsx
@@ -1,6 +1,6 @@
 export default function VideoSec() {
     return(<div className="my-11 mx-auto w-full max-w-6xl flex flex-col items-center">
-        <div className="text-6xl font-bold">What is Placed?</div>
+        <div className="text-3xl sm:text-6xl font-bold text-center">What is Placed?</div>
         <div className="p-6 w-full">
         <iframe width="560" className="w-[80%] mx-auto h-[300px] sm:h-[500px]" height="500" src="https://www.youtube.com/embed/X6CsbhSVUEc?si=tmi-RFpeDDVjJ4Y5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
