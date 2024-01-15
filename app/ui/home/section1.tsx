0abf693ca87f6167978b33774c87900c5b9028c3
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-01-15 16:25:53 +0530

    Changes in course listing section

--- app/ui/home/section1.tsx
+++ app/ui/home/section1.tsx
@@ -1,18 +1,21 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Section1() {
-    return(<div className="justify-center my-11 mx-auto">
-    <div className="flex flex-col sm:flex-row w-full mx-auto max-w-6xl items-stretch">
-      <div className="px-11">
-          <div className="text-5xl font-bold">Data Science Placement Courses</div>
-      </div>
-      <div className="bg-[#ff9635] shrink-0 w-1"></div>
-      <div className="flex flex-col justify-between lg:flex-row px-11 ">
-      <div className="text-xl flex items-center text-[#828181] my-6 sm:my-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum accusantium saepe </div>
-      <div className="flex items-center shrink-0">
-          <Link href="/course/" className="bg-[#ff9635] text-white text-xl py-2 px-6 rounded-3xl no-wrap">View Courses</Link>
-      </div>
+  return (
+    <div className="justify-center my-11 mx-auto">
+      <div className="flex flex-col sm:flex-row w-full mx-auto max-w-6xl items-stretch">
+        <div className="px-11">
+          <div className="text-5xl font-bold">
+            Assured Placement Courses
+          </div>
+        </div>
+        <div className="bg-[#ff9635] shrink-0 w-1"></div>
+        <div className="flex flex-col justify-between lg:flex-row px-11 ">
+          <div className="text-xl flex items-center text-[#828181] my-6 sm:my-0">
+          100% Assured Placement Course with No questions asked 100% Money Back guarantee in Data Science. Learn from the best in the society and pay only when you are Placed!
+          </div>
+        </div>
       </div>
     </div>
-  </div>)
+  );
 }
--- app/ui/home/section3.tsx
+++ app/ui/home/section3.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Section3() {
     return (<div className="grid grid-cols-[1fr] md:grid-cols-[2fr_1px_3fr] px-4 mx-auto max-w-6xl my-20">
-        <div className="text-3xl sm:text-5xl font-bold tracking-wider px-2 text-center md:text-left">Tech Stack you will Build</div>
+        <div className="text-3xl sm:text-5xl font-bold px-2 text-center md:text-left">Tech Stack you will Build</div>
         <div className="bg-[#ff9635] shrink-0 w-1 hidden md:block"></div>
         <div className="flex flex-nowrap justify-evenly items-stretc py-4 h-auto lg:h-24 w-full">
             <Image src="/Images/unnamed.png"   className="mx-2 w-1/6" width="100" height="100" alt="Tech Stack"/>
