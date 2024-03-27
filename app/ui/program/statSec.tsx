6aa3c1f059833fd46fcd31c4f03c99a70c04fc5a
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-03-27 20:47:15 +0530

    Animate numbers in Program Section

--- app/ui/program/statSec.tsx
+++ app/ui/program/statSec.tsx
@@ -1,19 +1,23 @@
-import Image from "next/image";
+import CountUp from "react-countup";
 
 export default function StatSec() {
   return (
     <div className="my-4 sm:my-11 mx-auto w-4/5 max-w-6xl">
       <div className="flex flex-row justify-evenly items-center">
         <div className="my-6 flex flex-col items-center justify-center">
-          <div className="text-2xl text-center sm:text-6xl font-bold">50+</div>
+          <div className="text-2xl text-center sm:text-6xl font-bold">
+            <CountUp start={0} end={50} duration={2} delay={0.4}/>+</div>
           <div className="text-center text-sm text-gray-500">Lessons</div>
         </div>
         <div className="my-6 flex flex-col items-center justify-center">
-          <div className="text-2xl text-center sm:text-6xl font-bold">10+</div>
+          <div className="text-2xl text-center sm:text-6xl font-bold">
+            <CountUp start={0} end={10} duration={2} delay={0.4}/>+</div>
           <div className="text-center text-sm text-gray-500">Hours of learning</div>
         </div>
         <div className="my-6 flex flex-col items-center justify-center">
-          <div className="text-2xl text-center sm:text-6xl font-bold">6</div>
+          <div className="text-2xl text-center sm:text-6xl font-bold">
+            <CountUp start={0} end={6} duration={2} delay={0.4}/>
+          </div>
           <div className="text-center text-sm text-gray-500">Series</div>
         </div>
       </div>
