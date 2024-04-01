87ecbfb32ac21f64e4d00f888aa177433e9820cb
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-04-01 19:03:48 +0530

    Add more members in "Team Page"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         "program-background": "url('/Images/get_in_touch_wallpaper.jpg')",
         "sec4-bg": "url('/Images/background1.jpg')",
         "get-touch-wall": "url('/Images/get_in_touch_wallpaper.jpg')",
-        "team-page": "url('/Icons/team_background.svg')",
+        "team-page": "url('/Images/team_background.svg')",
         "login-wall": "url('/Images/login_wall.jpg')",
         // 'login-wall' : "linear-gradient(140deg, #008080, #aaa 20%, #eee 40%, #aaa 50%, #007070)"
       },
