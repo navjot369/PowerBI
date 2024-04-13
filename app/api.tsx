29c511caa89c7860be3ceb9ff8f3914aef8b1038
harshit0571 <harshitchadha65@gmail.com>
2024-04-13 11:33:56 +0530

    api changed

--- app/api.tsx
+++ app/api.tsx
@@ -1,7 +1,7 @@
 // export const apiLink = "http://13.201.47.128:9000";
 
-// export const apiLink = "https://course-managment-api.vercel.app";
-export const apiLink = "http://localhost:9000";
+export const apiLink = "https://course-managment-api.vercel.app";
+// export const apiLink = "http://localhost:9000";
 
 //top is for production
 //bottom for local
